40eecb25018dc458c2e9df7f169ad2655dfab288
Amanhui <pamanhuipg@gmail.com>
2023-03-08 13:19:20 +0000

    Adds custom form validator to check expression validity

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { Subject, takeUntil, tap } from 'rxjs';
-import { degreesToRadians } from '../../app.utils';
-import { Constants } from '../../app.constants';
+import { ValidationErrors, AbstractControl, ValidatorFn, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Subject, takeUntil, map } from 'rxjs';
+import { ExpressionEvaluatorService } from 'src/app/services/expression-evaluator/expression-evaluator.service';
 
 interface IExpressionForm {
   expression: FormControl<string | null>;
@@ -21,22 +20,41 @@ export class CalculatorComponent implements OnInit, OnDestroy {
 
   constructor(
     private _fb: FormBuilder,
+    private _expressionEvaluator: ExpressionEvaluatorService,
   ) {
-    this.form = this._fb.group<IExpressionForm>( {
-      expression: this._fb.control<string>(''),
+    this.form = this._fb.group<IExpressionForm>({
+      expression: this._fb.control<string>('', {
+        validators: [
+          this.mathExpressionValidator()
+        ]
+      }),
     })
 
   }
+
   ngOnInit() {
     this.form.get('expression')?.valueChanges.pipe(
-      tap(c => console.log(c)),
+      map(s => this._expressionEvaluator.evaluateMathExpression(s as string)),
       takeUntil(this._destroy),
-    ).subscribe();
+    ).subscribe(res => (this.result = res));
   }
 
   ngOnDestroy(): void {
-      this._destroy.next(true);
-      this._destroy.unsubscribe();
+    this._destroy.next(true);
+    this._destroy.unsubscribe();
+  }
+
+  mathExpressionValidator(): ValidatorFn {
+
+    return (control: AbstractControl): ValidationErrors | null => {
+      return this._expressionEvaluator.isExpressionValid(control.value) ? null : { invalidExpression: true };
+    }
+  }
+
+
+
+  get expression() {
+    return this.form.get('expression');
   }
 
 }
